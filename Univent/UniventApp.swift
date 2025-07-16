2c65e583b155cf5062d90dea4cc049ed6972d7ca
AbhishekBalija <abhiarsikere0001@gmail.com>
2025-07-16 18:14:56 +0530

    Initial Commit

---
+++ Univent/UniventApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniventApp.swift
+//  Univent
+//
+//  Created by Abhishek AN on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct UniventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
